dda20f58cb72f250a41c25b64fc831f941bd2c89
Paul Cowan <paul@custardsource.com>
2023-04-13 21:02:24 +1000

    Very basic MQTT autodiscovery

--- config_flow.py
+++ config_flow.py
@@ -12,6 +12,8 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_registry import EntityRegistry
 from homeassistant.helpers.selector import selector
 import voluptuous as vol
+from homeassistant.helpers.config_entry_flow import DiscoveryFlowHandler
+from homeassistant.helpers.service_info.mqtt import MqttServiceInfo
 
 from .const import DEFAULT_NAME, DOMAIN
 from .sensor import (
@@ -50,7 +52,9 @@ def build_schema(
     config_entry: config_entries | None,
     hass: HomeAssistant,
     show_advanced: bool = False,
+    mqtt_auto: bool = False,
     step: str = "user",
+    detected_name: str = DEFAULT_NAME,
 ) -> vol.Schema:
     """Build configuration schema.
 
@@ -65,25 +69,30 @@ def build_schema(
     schema = vol.Schema(
         {
             vol.Required(
-                CONF_NAME, default=get_value(config_entry, CONF_NAME, DEFAULT_NAME)
-            ): str,
-            vol.Required(
-                CONF_ADSB_SENSOR, default=get_value(config_entry, CONF_ADSB_SENSOR, "")
-            ): selector({
-                "entity": {
-                    "filter": {
-                        "domain": "sensor",
-                    },
-                },
-            }),
-            vol.Optional(
-                CONF_ADSB_JSON_ATTRIBUTE,
-                default=get_value(
-                    config_entry, CONF_ADSB_JSON_ATTRIBUTE, JSON_ATTRIBUTE_DEFAULT
-                ),
+                CONF_NAME, default=get_value(config_entry, CONF_NAME, detected_name)
             ): str,
         },
     )
+    if not mqtt_auto:
+        schema = schema.extend(
+            {
+                vol.Required(
+                    CONF_ADSB_SENSOR, default=get_value(config_entry, CONF_ADSB_SENSOR, "")
+                ): selector({
+                    "entity": {
+                        "filter": {
+                            "domain": "sensor",
+                        },
+                    },
+                }),
+                vol.Optional(
+                    CONF_ADSB_JSON_ATTRIBUTE,
+                    default=get_value(
+                        config_entry, CONF_ADSB_JSON_ATTRIBUTE, JSON_ATTRIBUTE_DEFAULT
+                    ),
+                ): str,
+            },
+        )
     if show_advanced:
         schema = schema.extend(
             {
@@ -150,6 +159,69 @@ class AdsbInfoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     def async_get_options_flow(config_entry):
         """Get the options flow for this handler."""
         return AdsbInfoOptionsFlow(config_entry)
+
+    async def async_step_mqtt(self, discovery_info: MqttServiceInfo) -> FlowResult:
+        """Handle a flow initialized by MQTT discovery"""
+        _LOGGER.debug( f"Starting MQTT discovery for adsb_info with {discovery_info.topic}")
+        self._prefix = discovery_info.topic
+        stripped_prefix = self._prefix.replace("adsb/", "")
+        self._server = stripped_prefix
+        unique_id = f"{DOMAIN}-mqtt-{stripped_prefix}"
+
+        existing_ids = self._async_current_ids()
+        if unique_id in existing_ids:
+            _LOGGER.debug(
+                f"[{self._prefix}] ignoring because it has already been configured"
+            )
+            return self.async_abort(reason="instance_already_configured")
+
+        _LOGGER.info(f"Discovered new adsb_info integration for topic {discovery_info.topic}, will use ID '{unique_id}'")
+        await self.async_set_unique_id(unique_id)
+        return await self.async_step_confirm()
+
+    async def async_step_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Confirm setup to user and create the entry"""
+
+        _LOGGER.warn(f"Step_confirm:")
+        if not self._prefix:
+            return self.async_abort(reason="unsupported_manual_setup")
+
+        data = {"discovery_prefix": self._prefix,
+                }
+
+        if user_input is None:
+            self.context["title_placeholders"] = {"prefix": self._prefix, "server": self._server}
+            schema = build_schema(
+                config_entry=None,
+                hass=self.hass,
+                mqtt_auto=True,
+                show_advanced=False,
+                detected_name=self._server,
+            )
+            return self.async_show_form(
+                step_id="confirm",
+                data_schema=schema,
+                description_placeholders={
+                    "discovery_topic": self._prefix,
+                    "prefix": self._prefix,
+                    "server": self._server,
+                },
+            )
+
+        #return self.async_abort(reason="instance_already_configured")
+        # TODO
+        data = user_input | {
+                'mqtt_topic': self._prefix,
+                'data_source': 'MQTT',
+        }
+        _LOGGER.warn(f"Going to use user_data {data}")
+
+        return self.async_create_entry(
+            title=user_input[CONF_NAME],
+            data=data,
+        )
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
